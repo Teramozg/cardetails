eea393f160be60c07bc5235faff9931f7e69e6fa
teramozg <sermomotouk@gmail.com>
2023-11-19 10:51:44 +0300

    initialcommit

---
+++ src/main/java/com/serg/cardetails/CardetailsApplication.java
@@ -0,0 +1,13 @@
+package com.serg.cardetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serg/cardetails/CardetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serg.cardetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
